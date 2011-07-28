d2c8a9a77a4eb57d51209981d936a2503ffc511f
sahil chug <sahil2422@gmail.com>
2011-07-29 01:50:32 +0530

    updated and added helpers.py

--- PyGame.py
+++ PyGame.py
@@ -1,3 +1,4 @@
+from helpers import *
 import os , sys
 import pygame
 from pygame.locals import *
@@ -20,10 +21,27 @@ class PyManMain:
 
     def MainLoop(self):
 	"""This is the Main Loop of the game"""
+	"""Load All of our sprites"""
+	self.LoadSprites()
 	while 1:
 	    for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 		    sys.exit()
+
+	    self.snake_sprites.draw(self.screen)
+	    pygame.display.flip()
+
+    def LoadSprites(self):
+	""" Load the sprite we need"""
+	self.snake = Snake()
+	self.snake_sprites = pygame.sprite.RenderPlain((self.snake))
+class Snake(pygame.sprite.Sprite):
+    """This is out snake that will be moved around the screen"""
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+	self.image,self.rect=load_image('snake.png',-1)
+	self.pellets =0
 
 if __name__ == "__main__":
     MainWindow = PyManMain()
